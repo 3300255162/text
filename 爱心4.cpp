b1f465a3bd4a954244c10a7f3cabf9848be092c4
3300255162 <152685653+3300255162@users.noreply.github.com>
2023-12-03 20:41:27 +0800

    Add files via upload

---
+++ 爱心4.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <conio.h>
+//int main()
+//{
+//	for (float y = 1.5f; y > -1.5f; y -= 0.1f)
+//	{
+//		for (float x = -1.5f; x < 1.5f; x += 0.05f)
+//		{
+//			float a = x * x + y * y - 1;
+//			putchar(a * a * a - x * x * y * y * y <= 0.0f ? '*' : ' ');
+//		}
+//		putchar('\n');
+//	}
+//	for (; !kbhit();)    //kbhit()位于conio.h中，检测是否有键按下
+//	{
+//		char str[10];
+//		sprintf(str, "color %x", rand() % 16); //形成一条color X的字符串，注意要用%x
+//		system(str);
+//		sleep(0.5);
+//	}
+//	return 0;
+//}
